f27794e778a115ad46451315f95e23e05eb99718
Jeb Phillips <phillipsjl1@appstate.edu>
2020-08-04 18:13:07 -0400

    new code changes

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -5,119 +5,93 @@ import './graph.css';
 
 class Graph extends Component {
   state = {
-    mockData: [],
+    data: null,
   }
-
-   tempMockData = [
-    {water: 1, calories: 23, steps:2},
-    {water: 1, calories: 23, steps:2},
-    {water: 1, calories: 23, steps:2},
-    {water: 1, calories: 23, steps:2},
-    {water: 1, calories: 23, steps:2},
-    {water: 1, calories: 23, steps:2},
-    {water: 1, calories: 23, steps:2},
-  ];
 
   componentDidMount() {
-
+    fetch("https://enigmatic-shelf-30666.herokuapp.com/fitnessprogress/d7eeaa96-89ec-402c-a071-fc6f8b85e039")
+      .then(res => res.json())
+      .then(
+        (rawData) => {
+          console.log(rawData);
+          this.setState({
+            data: this.parseData(rawData)
+          });
+        });
   }
+
+  parseData(rawData) {
+
+    const maximums = rawData.reduce((maxes, value) => {
+      return {
+        water: maxes.water < value.water ? value.water : maxes.water,
+        steps: maxes.steps < value.steps ? value.steps : maxes.steps,
+        calories: maxes.calories < value.calories ? value.calories : maxes.calories
+      };
+    }, {
+      water: 0,
+      steps: 0,
+      calories: 0
+    });
+
+    return rawData.reduce((data, value) => {
+      data.water.push({
+        x: value.created,
+        y: value.water / maximums.water
+      });
+
+      data.steps.push({
+        x: value.created,
+        y: value.steps / maximums.steps
+      });
+
+      data.calories.push({
+        x: value.created,
+        y: value.calories / maximums.calories
+      });
+
+      return data;
+    }, {
+      water: [],
+      steps: [],
+      calories: []
+    });
+  }
+
   render(){
 
-    const graphData = [
-      {x: "Monday", y: 2},
-      {x: "Tuesday", y: 3},
-      {x: "Wednesday", y: 1},
-      {x: "Thursday", y: 4},
-      {x: "Friday", y: 3},
-      {x: "Saturday", y: 1},
-      {x: "Sunday", y: 1},
-    ];
-
-    const graphData2 = [
-      {x: "Monday", y: 3},
-      {x: "Tuesday", y: 3},
-      {x: "Wednesday", y: 3},
-      {x: "Thursday", y: 3},
-      {x: "Friday", y: 3},
-      {x: "Saturday", y: 3},
-      {x: "Sunday", y: 3},
-    ];
-
-<<<<<<< Updated upstream
-    const stepsData = [
-      {x: "Monday", y: 23},
-      {x: "Tuesday", y: 9},
-      {x: "Wednesday", y: 7},
-      {x: "Thursday", y: 15},
-      {x: "Friday", y: 2},
-      {x: "Saturday", y: 1},
-      {x: "Sunday", y: 1},
-    ];
-
-    const stepsData2 = [
-      {x: "Monday", y: 4},
-      {x: "Tuesday", y: 4},
-      {x: "Wednesday", y: 4},
-      {x: "Thursday", y: 4},
-      {x: "Friday", y: 4},
-      {x: "Saturday", y: 4},
-      {x: "Sunday", y: 4},
-    ];
-
-    const calorieData = [
-      {x: "Monday", y: 5},
-      {x: "Tuesday", y: 3},
-      {x: "Wednesday", y: 1},
-      {x: "Thursday", y: 10},
-      {x: "Friday", y: 15},
-      {x: "Saturday", y: 7},
-      {x: "Sunday", y: 12},
-    ];
-
-    const calorieData2 = [
-      {x: "Monday", y: 7},
-      {x: "Tuesday", y: 7},
-      {x: "Wednesday", y: 7},
-      {x: "Thursday", y: 7},
-      {x: "Friday", y: 7},
-      {x: "Saturday", y: 7},
-      {x: "Sunday", y: 7},
-    ];
-
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
-=======
-    const mockData = [
-      {id: "0", water: "1", calories: "500", steps:"123"},
-      {id: "1", water: "22", calories: "400", steps:"222"},
-      {id: "2", water: "4", calories: "230", steps:"59"},
-
-    ];
->>>>>>> master
     const graphWidth = 900;
     const graphHeight = 300;
-    const graphDomain = [0, 10];
+    const graphDomain = [0, 1];
+
+    if(!this.state.data) return <div></div>;
 
     return(
       <div>
         <XYPlot xType="ordinal" width={graphWidth} height={graphHeight} yDomain={graphDomain}>
           <XAxis />
-              
+
           <LineSeries
               color = "#316fd5"
               style={{strokeWidth: 3}}
-              data={graphData2} />
+              data={this.state.data.water} />
           <VerticalBarSeries
-              color = "#3bb1c1"
-              data = {this.tempMockData} />
+              color = "#316fd5"
+              data = {this.state.data.water} />
           <LineSeries
               color = "#3bb1c1"
               style={{strokeWidth: 3}}
-              data={stepsData2} />
+              data={this.state.data.steps} />
+          <VerticalBarSeries
+              color = "#3bb1c1"
+              data={this.state.data.steps} />
+          <LineSeries
+              color = "#ee3769"
+              style={{strokeWidth: 3}}
+              data={this.state.data.calories} />
           <VerticalBarSeries
               color = "#ee3769"
-              data={this.tempMockData} /> 
+              data={this.state.data.calories} />
 
 
           <DiscreteColorLegend
