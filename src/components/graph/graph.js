737ad796ba246a6d46dc571844a6093e3e2cbe9d
Madeline <madeline.sides@ally.com>
2020-08-04 09:51:57 -0400

    3 bar graph

--- src/components/graph/graph.js
+++ src/components/graph/graph.js
@@ -24,9 +24,50 @@ class Graph extends Component {
       {x: "Saturday", y: 3},
       {x: "Sunday", y: 3},
     ];
+
+    const stepsData = [
+      {x: "Monday", y: 5},
+      {x: "Tuesday", y: 7},
+      {x: "Wednesday", y: 1},
+      {x: "Thursday", y: 4},
+      {x: "Friday", y: 2},
+      {x: "Saturday", y: 1},
+      {x: "Sunday", y: 1},
+    ];
+
+    const stepsData2 = [
+      {x: "Monday", y: 4},
+      {x: "Tuesday", y: 4},
+      {x: "Wednesday", y: 4},
+      {x: "Thursday", y: 4},
+      {x: "Friday", y: 4},
+      {x: "Saturday", y: 4},
+      {x: "Sunday", y: 4},
+    ];
+
+    const calorieData = [
+      {x: "Monday", y: 2},
+      {x: "Tuesday", y: 3},
+      {x: "Wednesday", y: 1},
+      {x: "Thursday", y: 4},
+      {x: "Friday", y: 3},
+      {x: "Saturday", y: 1},
+      {x: "Sunday", y: 1},
+    ];
+
+    const calorieData2 = [
+      {x: "Monday", y: 3},
+      {x: "Tuesday", y: 3},
+      {x: "Wednesday", y: 3},
+      {x: "Thursday", y: 3},
+      {x: "Friday", y: 3},
+      {x: "Saturday", y: 3},
+      {x: "Sunday", y: 3},
+    ];
+
     const graphWidth = 900;
     const graphHeight = 300;
-    const graphDomain = [0, 5];
+    const graphDomain = [0, 10];
 
     return(
       <div>
@@ -34,7 +75,9 @@ class Graph extends Component {
                 <XAxis />
                 <YAxis />
           <VerticalBarSeries data={graphData} />
-          <LineSeries data={graphData2} />
+          <VerticalBarSeries data = {stepsData} />
+          <VerticalBarSeries data={calorieData} />
+          <LineSeries data={graphData2} data={stepsData2}/>
           <LabelSeries
                     data={graphData.map(obj => {
                         return { ...obj, label: obj.y.toString() }
